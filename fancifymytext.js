00a9c9c97d315187d4dce277e7e9bfb91cecc4ce
Angelina Castillo <angelina.rose.ca@gmail.com>
2023-10-28 02:26:52 -0700

    added helloAlert function

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,6 +1,10 @@
 
 const biggrBtn = document.querySelector("#biggerButton");
 
+function helloAlert(){
+    alert("Hello World!");
+}
+
 biggrBtn.addEventListener("click", () => {
-  alert("Hello World!");
+  helloAlert();
 });

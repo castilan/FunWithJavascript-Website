b66e442721e0519adfa95b0bbcbfcc5cd06d6bac
Angelina Castillo <angelina.rose.ca@gmail.com>
2023-11-02 01:41:15 -0700

    added makeFancy() and event handler for fancy

--- fancifymytext.js
+++ fancifymytext.js
@@ -3,8 +3,11 @@ alert("Hello, world!");
 
 // shows a pop up with "Hello World!" 
 //assigned to the Bigger button
-function helloAlert(){
+function makeBigger(){
     document.getElementById("innerText").style.fontSize = '31.92px';
 }
 
+function makeFancy(){
+    document.getElementById("innerText").style.fontFamily = 'cursive';
+}
 

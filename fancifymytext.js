16e1cc73335ae0cac8b58e307eb9c3218ea3cedd
Angelina Castillo <angelina.rose.ca@gmail.com>
2023-11-02 00:50:43 -0700

    Corrected id in js file

--- fancifymytext.js
+++ fancifymytext.js
@@ -4,7 +4,7 @@ alert("Hello, world!");
 // shows a pop up with "Hello World!" 
 //assigned to the Bigger button
 function helloAlert(){
-    document.getElementById("textArea").style.fontSize = '31.92px';
+    document.getElementById("innerText").style.fontSize = '31.92px';
 }
 
 

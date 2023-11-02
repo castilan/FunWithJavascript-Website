557a836eabb532df4c07561a64221f063e5c2235
Angelina Castillo <angelina.rose.ca@gmail.com>
2023-11-02 11:27:52 -0700

    another one

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,16 +1,16 @@
 
 alert("Hello, world!");
 
-const innerText = document.getElementById("innerText");
+const text = document.getElementById("innerText");
 
 // shows a pop up with "Hello World!" 
 //assigned to the Bigger button
 function makeBigger(){
-    innerText.style.fontSize = '31.92px';
+    text.style.fontSize = '31.92px';
 }
 
 function makeFancy(){
-    innerText.style.fontFamily = 'Courier New', 'monospace';
-    innerText.style.backgroundColor = 'red';
+    text.style.fontFamily = 'Courier New', 'monospace';
+    text.style.backgroundColor = 'red';
 }
 
